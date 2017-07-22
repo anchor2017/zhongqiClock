9f2a88cfbc170c8d96d3155ec0c477dfc4fa8f32
anchor <962353195@qq.com>
2017-07-22 20:48:27 +0800

    v1.4 version

--- main.c
+++ main.c
@@ -16,23 +16,23 @@
 
 void main()
 {
-	uchar h1[1]={0};
-	uchar h0[1]={0};
-	uchar m1[1]={0};
-	uchar m0[1]={0};
-	uchar s1[1]={0};
-	uchar s0[1]={0};	//存放时分秒
-	uchar hour1=0, hour0=0, min1=0, min0=0, sec1=0, sec0=0;
+	uchar h1[]={0,0,0};
+	uchar h0[]={0,0,0};
+	uchar m1[]={0,0,0};
+	uchar m0[]={0,0,0};
+	uchar s1[]={0,0,0};
+	uchar s0[]={0,0,0};	//存放时分秒
+	uchar hour1=2, hour0=0, min1=4, min0=4, sec1=3, sec0=5;
 	uchar Time[]={0, 0, 0, 0, 0, 0};
 	
 	uchar i=0;		//i与Time数组有关
-	uint x=0, y=0;	//存放x,y坐标
+	long x=0, y=0;	//存放x,y坐标
 	uchar set=0, back=0;  //设置时间，返回主时钟
 	
 	h1[0]=hour1+'0';
-	//h0[0]=hour0+'0';
-	//m1[0]=min1+'0';
-	//m0[0]=min0+'0';
+	h0[0]=hour0+'0';
+	m1[0]=min1+'0';
+	m0[0]=min0+'0';
 	s1[0]=sec1+'0';
 	s0[0]=sec0+'0';
 
@@ -44,22 +44,29 @@ void main()
 
 	//GUI_WriteASCII2(20, 100, h1, 0xF800, 0x0000);
 
-	//GUI_WriteASCII(20, 100, "3", 0xF800, 0x0000);	 //h1			 	
-	//GUI_WriteASCII(36, 170, h0, 0xF800, 0x0000);	 //h0				 	
-	//GUI_WriteASCII(46, 170, m1, 0xF800, 0x0000);	 //m1			 	
-	//GUI_WriteASCII(62, 170, m0, 0xF800, 0x0000);	 //m0				 	
-	GUI_WriteASCII(72, 170, s1, 0xF800, 0x0000);	 //s1			 	
-	//GUI_WriteASCII(88, 170, s0, 0xF800, 0x0000);	 //s0			 	
+	GUI_WriteASCII2(10, 100, h1, 0xF800, 0x0000);	 //h1			 	
+	GUI_WriteASCII2(30, 100, h0, 0xF800, 0x0000);	 //h0
+
+	GUI_WriteASCII2(50, 94, ":", 0xF800, 0x0000);
+					 	
+	GUI_WriteASCII2(70, 100, m1, 0xF800, 0x0000);	 //m1			 	
+	GUI_WriteASCII2(90, 100, m0, 0xF800, 0x0000);	 //m0
+
+	GUI_WriteASCII2(110, 94, ":", 0xF800, 0x0000);
+					 	
+	GUI_WriteASCII2(130, 100, s1, 0xF800, 0x0000);	 //s1			 	
+	GUI_WriteASCII2(150, 100, s0, 0xF800, 0x0000);	 //s0
+				 	
 	GUI_WriteASCII(127, 0, "SET", 0x001F, 0x0000);		 //设置按钮
 
 	 while(1)
 	 {
 		
-		/*时钟界面*/
+		/*返回时钟界面*/
 		if(back==1)
 		{
 			TFT_ClearScreen(0x0000);
-			GUI_ShowPicture(0, 0, 150, 150);
+			//GUI_ShowPicture(0, 0, 150, 150);
 			GUI_WriteASCII(20, 94, h1, 0xF800, 0x0000);	 //h1			 	
 			GUI_WriteASCII(36, 94, h0, 0xF800, 0x0000);	 //h0
 						 	
@@ -68,12 +75,13 @@ void main()
 						 	
 			GUI_WriteASCII(72, 94, s1, 0xF800, 0x0000);	 //s1			 	
 			GUI_WriteASCII(88, 94, s0, 0xF800, 0x0000);	 //s0			 	
-			GUI_WriteASCII(127, 0, "SET", 0x001F, 0x0000);		 //设置按
+			GUI_WriteASCII(127, 0, "SET", 0x001F, 0x0000);	 //设置按扭
 		}
 
-		/*设置界面*/
+		/*进入设置界面*/
 		if(set==1)		
 		{
+			
 			TFT_ClearScreen(0x0000);
 			GUI_WriteASCII(47, 24, "CLOCK", 0xFFE0, 0x0000);	  //x, y	
 		  	//横线
@@ -96,11 +104,12 @@ void main()
 			GUI_WriteASCII(81, 174, "8", 0xF800, 0x0000);	 //8
 			GUI_WriteASCII(127, 174, "9", 0xF800, 0x0000);	 //9
 		    GUI_WriteASCII(127, 58, "0", 0xF800, 0x0000);	 //0	
-			GUI_WriteASCII(111, 0, "BACK", 0x001F, 0x0000);	 
+			GUI_WriteASCII(111, 0, "BACK", 0x001F, 0x0000);	
+			 
 			set = 0;
 		}
 
-		if(TOUCH_XPT_ReadXY()==1)
+		if(TOUCH_XPT_ReadXY() == 1)
 		{
 			/*转化*/
 			x=xpt_xy.x;
@@ -117,10 +126,11 @@ void main()
 				y=218;
 			}
 			
+
 			/*屏上数字*/
 			if((x>115)&&(y<24))
 			{
-				back=1;
+				set=1;
 			}
 			if((x>115)&&(y<45)&&(y>85))	  //0
 			{
