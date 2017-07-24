a38606f7bf9fbad8d81f2bfa0b9603979b20aff5
anchor <962353195@qq.com>
2017-07-24 11:35:34 +0800

    add // in main.c

--- main.c
+++ main.c
@@ -14,9 +14,9 @@
 
 uchar TimeArray[]={0, 0, 0, 0, 0, 0};
 uchar i=0;
-
-uchar anotherSur=1;		//可以先设置为1，第一次需要清屏(之后只是时间计数就不需要再次清屏)，在calTimeDisplay.h中声明为全局变量	
-long x=0, y=0;	
+//
+uchar anotherSur=1;		//可以先设置为1，第一次需要清屏(之后只是时间计数就不需要再次清屏)，在calTimeDisplay.h中声明为全局变量
+long x=0, y=0;
 uchar set=0, back=1;  //设置标志位，返回标志位（先置1才能运行），全局变量在calTimeDisplay.h中声明
 uchar setFlag=0, clockFlag=0;	//防止出现在一个界面按键却触发另外一个界面内容的错误
 uchar oriTime=0;			 //修改时间错误时原来的时间
@@ -44,4 +44,3 @@ void main()
 
 }
 
-		
